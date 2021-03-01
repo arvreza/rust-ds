0852ba0ffb4d77f2f5e08d51ce670f547988179f
arvreza <32081264+arvreza@users.noreply.github.com>
2021-03-01 01:13:25 -0800

    adding box section

---
+++ v1_box/src/main.rs
@@ -0,0 +1,41 @@
+#[derive(Debug)]
+pub struct LinkedList<T> {
+    data: T,
+    /*
+    All values in Rust are stack allocated by default. 
+    Values can be boxed (allocated on the heap) by creating a Box<T>. 
+    A box is a smart pointer to a heap allocated value of type T. 
+    When a box goes out of scope, its destructor is called, 
+    the inner object is destroyed, and the memory on the heap is freed.
+    */
+    next: Option<Box<LinkedList<T>>>,
+}
+
+impl<T: std::ops::AddAssign> LinkedList<T> {
+    pub fn add_up(&mut self, n: T) {
+        self.data += n;
+    }
+}
+
+fn main() {
+    let mut list = LinkedList {
+        data: 3,
+        next: Some(Box::new(LinkedList {
+            data: 2,
+            next: None,
+        })),
+    };
+
+    if let Some(ref mut v) = list.next {
+        v.add_up(10);
+    }
+
+    let mut v: Vec<String> = Vec::with_capacity(4);
+
+    v.push("hello".to_string());
+    v.push("goodbye".to_string());
+
+    println!("v.len = {}, capacity = {}", v.len(), v.capacity());
+
+    println!("Our list is equal: {:?}", list);
+}
